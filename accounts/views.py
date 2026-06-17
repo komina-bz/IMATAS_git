1ada3eae8107f563566de843e5a6b50546b1200d
komina <komina.bz@gmail.com>
2026-06-17 17:37:59 +0900

    登録画面新規作成中、モデル未作成

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .forms import UserForm
 
 def login_view(request):
     return render(request, 'accounts/login.html')
+
+def regist(request):
+    user_form = UserForm(request.POST or None)
+    return render(request, 'accounts/registration.html', context={
+        'user_form': user_form,
+    })
