aab3e627530ece1ce4688fa3fc50ae52ab4f4e75
komina <komina.bz@gmail.com>
2026-06-18 01:38:36 +0900

    Userモデル修正

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-class users(models.Model):
+class Users(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=255)
     password = models.CharField(max_length=50)
-    remind_enabled = models.IntegerField()     # 0:OFF, 1:ON
-    remind_before_days = models.IntegerField() # 何日前に通知するか
-    remind_time = models.TimeField()           # 通知時間
+    remind_enabled = models.IntegerField(default=0)     # 0:OFF, 1:ON
+    remind_before_days = models.IntegerField(default=0) # 何日前に通知するか
+    remind_time = models.TimeField(default="00:00:00")  # 通知時間
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
-        db_table = "users"
+        db_table = "Users"
