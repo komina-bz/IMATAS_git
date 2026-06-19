28c63b637de73e2d9d519812ba6a15a6b395162c
komina <komina.bz@gmail.com>
2026-06-20 01:59:51 +0900

    タスク一覧ページ新規作成、フッターリンク追加（マイページ以外）

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "tasks"
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('task_list/', views.task_list, name='task_list'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'tasks/home.html')
+
+def task_list(request):
+    return render(request, 'tasks/task_list.html')
