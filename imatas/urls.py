937e37903831e80cac18e2325f83954a426e4e8e
komina <komina.bz@gmail.com>
2026-06-20 00:27:40 +0900

    homeページ作成

--- imatas/urls.py
+++ imatas/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('portfolio/', include('portfolio.urls')),
     path('accounts/', include('accounts.urls')),
+    path('tasks/', include('tasks.urls')),
 ]
