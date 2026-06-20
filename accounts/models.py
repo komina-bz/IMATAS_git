081f52244d5de693fb9731d9a6be33acd84c697a
komina <komina.bz@gmail.com>
2026-06-21 01:06:53 +0900

    usersモデル変更

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Users(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=255)
     password = models.CharField(max_length=50)
@@ -12,4 +11,7 @@ class Users(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
-        db_table = "Users"
+        db_table = "users"
+        
+    def __str__(self):
+        return self.name        
