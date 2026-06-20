e460a4633ddcb89155b5706152a9f2c9f6c40a7e
komina <komina.bz@gmail.com>
2026-06-21 01:05:51 +0900

    tasksモデル作成

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from accounts.models import Users
 
-# Create your models here.
+class Tasks(models.Model):
+    user = models.ForeignKey(
+        Users, on_delete=models.CASCADE, related_name="tasks")
+    parent_task = models.ForeignKey(
+        "self", on_delete=models.CASCADE, 
+        null=True, blank=True, related_name="subtasks"
+        )
+    name = models.CharField(max_length=100)
+    memo = models.TextField(null=True, blank=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    status = models.IntegerField(default=0)    # 0:未完了, 1:完了
+    display_order = models.IntegerField()      # タスク一覧表示順番
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = "tasks"
+        
+    def __str__(self):
+        return self.name
