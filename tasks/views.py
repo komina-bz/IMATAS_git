af8658d48e3db9c15c8db6fe47ae05396f0c904a
komina <komina.bz@gmail.com>
2026-06-22 00:35:25 +0900

    タスク詳細新規作成（タスク名、メモ、期限入力のみ）

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from accounts.utils import login_required_custom
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from . import forms
 from .models import Tasks
 from accounts.models import Users
@@ -38,3 +38,39 @@ def add_task(request):
     return render(request, 'tasks/add_task.html', context={
         'add_task_form': add_task_form,
     })
+    
+@login_required_custom
+def task_detail_view(request, task_pk):
+    # 登録データを取得
+    task_data = get_object_or_404(Tasks, pk=task_pk)
+    task_form = forms.TaskForm(instance=task_data)
+    
+    # フォームのすべてのフィールドを読み取り専用（readonly）にする
+    for field_name, field in task_form.fields.items():
+        field.widget.attrs['readonly'] = True
+        # Select（選択肢）やCheckboxはreadonlyが効かないため、
+        # 後述のCSS（pointer-events）で操作不能にします。
+        # 必要に応じて、ここで「disabled」を付けてもCSSで見た目を上書き可能です。        
+    
+    return render(request, 'tasks/task_detail.html', context={
+        'task_form': task_form,
+        'task_data': task_data
+    })
+
+@login_required_custom
+def edit_task(request, pk):
+    edit_task_form = forms.TaskForm(request.POST or None)
+    task_data = Tasks.objects.get(pk=pk)
+    # pkからデータを取得してフォームに表示
+    
+    if request.method == 'POST': 
+        # valid → 登録
+        if edit_task_form.is_valid():
+            task = edit_task_form.save(commit=False)    
+            task.save()  
+            return redirect('tasks:task_list') # 詳細画面に
+
+    return render(request, 'tasks/edit_task.html', context={
+        'edit_task_form': edit_task_form,
+    })
+    #return render(request, 'detail.html', {'data': data})
