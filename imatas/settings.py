dac5aca57473e75b3055ecc309821dae293dc845
komina <komina.bz@gmail.com>
2026-07-28 03:03:04 +0900

    cssファイル作成・style調整　base,home(途中)

--- imatas/settings.py
+++ imatas/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
