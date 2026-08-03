57adcf924c3e4bcbe0836a0d3562f911a8eb50b5
komina <komina.bz@gmail.com>
2026-08-04 01:11:38 +0900

    ポートフォリオ画面 作成

--- imatas/settings.py
+++ imatas/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio',
-    #'accounts',
     'accounts.apps.AccountsConfig',
     'tasks',
 ]
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "portfolio"
 urlpatterns = [
     path('', views.portfolio, name='portfolio'),
 ]
