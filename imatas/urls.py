130104fe2173223923c678a5df3b20ceec13e159
komina <komina.bz@gmail.com>
2026-08-29 01:17:50 +0900

    portfolioをトップページに

--- imatas/urls.py
+++ imatas/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('portfolio.urls')), 
     path('admin/', admin.site.urls),
     path('portfolio/', include('portfolio.urls')),
     path('accounts/', include('accounts.urls')),
