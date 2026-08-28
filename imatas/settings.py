564a815a69524bf7fd1fafbe4ee54f7f03ea6b69
komina <komina.bz@gmail.com>
2026-08-29 01:09:37 +0900

    ALLOWED_HOSTSを追加

--- imatas/settings.py
+++ imatas/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-t3gx1_74yen7taybuobo8d6^9!!x2!#*m5nsmr9*0-ji&jmo%r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'komina.pythonanywhere.com']
 
 
 # Application definition
