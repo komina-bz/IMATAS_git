d7a1aab53fd1dcecefa237f73bf95a85fb199e87
komina <komina.bz@gmail.com>
2026-08-28 17:00:35 +0900

    メールダミー

--- imatas/settings.py
+++ imatas/settings.py
@@ -130,7 +130,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "b5df9e001@smtp-brevo.com"
+EMAIL_HOST_USER = "dammy@smtp-brevo.com" # ダミー
 EMAIL_HOST_PASSWORD = "xxxxxx" # ダミー
 
 
